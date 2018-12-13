96ffe950d7da6a83fb0bc675d9147b258c70c740
alcanzer <joseph.stanton8558@gmail.com>
2018-12-13 11:47:43 -0600

    data

--- app/src/main/java/com/cogniaid/cogniaid/Constants/Constants.java
+++ app/src/main/java/com/cogniaid/cogniaid/Constants/Constants.java
@@ -5,7 +5,7 @@ package com.cogniaid.cogniaid.Constants;
  */
 
 public class Constants {
-    public static final String BUSINESS = "https://cogniwebhook.mybluemix.net/talk";
+    public static final String BUSINESS = "http://www.rediff.comgit ";
     public static final String AUTO_DEALER = "https://www.google.com";
     public static final String SURVEY = "https://www.yahoo.com";
 }
